af4f75ed302b80fb15932d4eb67410f9dd03a4a1
Guilherme Sella <guilhermesella1974@gmail.com>
2023-11-29 17:00:18 -0300

    adicionando modal para confirmação de logout

---
+++ TCCBarbearia/Scripts/ModalSairScript.js
@@ -0,0 +1,13 @@
+﻿const modal = document.querySelector(".modal-sair");
+const fundoPreto = document.querySelector(".fundo-preto");
+
+function openModal() {
+    
+    modal.classList.add("modal-open");
+    fundoPreto.classList.add("modal-open")
+}
+
+function fecharModal() {
+    modal.classList.remove("modal-open");
+    fundoPreto.classList.remove("modal-open");
+}
